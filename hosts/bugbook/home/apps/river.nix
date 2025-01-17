e239efc32a5fb4e7d3a9b6ce632f89ae66291724
inssekt <matthol11@outlook.com>
2025-01-17 03:28:40 +0000

    a

--- hosts/bugbook/home/apps/river.nix
+++ hosts/bugbook/home/apps/river.nix
@@ -6,4 +6,8 @@
   home-manager.users.bug.home.file = {
     ".config/river/" = { source = ../dots/river; target = ".config/river/"; };
   };
+
+  home-manager.users.bug.home.packages = with pkgs; [
+        rivercarro
+      ];
 }
--- hosts/bugbook/system/configuration.nix
+++ hosts/bugbook/system/configuration.nix
@@ -65,6 +65,8 @@
   console.keyMap = "uk";
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
+  programs.fish.enable = true;
+  users.defaultUserShell = pkgs.fish;
   users.users.bug = {
     isNormalUser = true;
     description = "bug";
@@ -75,11 +77,7 @@
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
   environment.systemPackages = with pkgs; [
-  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #  wget
 	wayland
 	xwayland
 	foot
@@ -88,32 +86,6 @@
 	dolphin
   libinput
   ];
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "24.11"; # Did you read the comment?
 
 }
